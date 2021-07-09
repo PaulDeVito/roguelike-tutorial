58e86fb15f3ec27629c4f2e4a092db6b2a04898a
Paul DeVito <pid7@cornell.edu>
2021-07-08 20:24:16 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import tcod
+
+from engine import Engine
+from entity import Entity
+from input_handlers import EventHandler
+
+
+def main() -> None:
+
+    screen_width = 80
+    screen_height = 50
+
+    tileset = tcod.tileset.load_tilesheet(
+        "chars.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    event_handler = EventHandler()
+
+    center_x = int(screen_width / 2)
+    center_y = int(screen_height / 2)
+    player = Entity(center_x, center_y, "@", (255, 255, 255))
+    npc = Entity(center_x - 5, center_y, "@", (255, 255, 0))
+    entities = {npc, player}
+
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
+
+
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset = tileset,
+        title = "The Dankest Roguelike",
+        vsync=True
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        while True:
+            engine.render(console=root_console ,context=context)
+
+            events = tcod.event.wait()
+
+            engine.handle_events(events)
+
+
+if __name__ == "__main__":
+    main()
